e948f9972f2cbdc68f87374849ac37ac59c4fe9c
ajmccredie <127118760+ajmccredie@users.noreply.github.com>
2023-05-08 20:51:35 +0000

    Initial layout of required card game functions in the script

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,33 @@
 console.log("Hello from the console");
+
+function loadGameButton() {
+
+}
+
+function createDeck() {
+
+}
+
+function shuffleDeck() {
+
+}
+
+function dealStartCards() {
+
+}
+
+function valueCards() {
+
+}
+
+function drawMore() {
+
+}
+
+function compareHands() {
+
+}
+
+function displayScore() {
+
+}
