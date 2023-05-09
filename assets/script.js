d0507024967cd218d830916176d23171ce1a2523
ajmccredie <>
2023-05-09 20:24:14 +0000

    Create a full deck of ordered cards

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,19 @@ function loadGameButton() {
 
 }
 
+createDeck();
+
 function createDeck() {
     var suits = ['hearts', 'diamonds', 'spades', 'clubs'];
     var cardValue = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    var orderedDeck = []
     
+    for(var suit = 0; suit < 4; suit++) {
+        for(var cardNum = 0; cardNum < 13; cardNum++) {
+            orderedDeck.push(cardValue[cardNum] + suits[suit]);
+        }
+    }
+    console.log(orderedDeck);
 }
 
 function shuffleDeck() {
