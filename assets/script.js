055cc37d53774993af360299107112a558aa139d
ajmccredie <>
2023-05-09 20:05:53 +0000

    Create initial card variables of suits and values in order to start creating the deck

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 console.log("Hello from the console");
 
+
 function loadGameButton() {
 
 }
 
 function createDeck() {
-
+    var suits = ['hearts', 'diamonds', 'spades', 'clubs'];
+    var cardValue = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    
 }
 
 function shuffleDeck() {
