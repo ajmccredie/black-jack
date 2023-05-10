f77f9d854db109830497101f4fda1f0b4edee58e
ajmccredie <>
2023-05-10 19:27:26 +0000

    Take the returned ordered deck from the create deck function and pass it to the shuffle function

--- assets/script.js
+++ assets/script.js
@@ -8,19 +8,25 @@ function loadGameButton() {
 createDeck();
 
 function createDeck() {
-    var suits = ['♥', '♦', '♠', '♣'];
-    var cardValue = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    const SUITS = ['♥', '♦', '♠', '♣'];
+    const CARDVALUE = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     var orderedDeck = []
     
     for(var suit = 0; suit < 4; suit++) {
         for(var cardNum = 0; cardNum < 13; cardNum++) {
-            orderedDeck.push(cardValue[cardNum] + suits[suit]);
+            orderedDeck.push(CARDVALUE[cardNum] + SUITS[suit]);
         }
     }
-    console.log(orderedDeck);
+    return orderedDeck
 }
 
-function shuffleDeck() {
+let gameDeck = createDeck();
+console.log(gameDeck);
+let shuffled = shuffleDeck(gameDeck);
+
+function shuffleDeck(gameDeck) {
+    var deckSize = gameDeck.length;
+    console.log(deckSize);
 
 }
 
