77a34bd5a41aef2a68b2a2da703189ea164e2f73
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-05-13 15:12:48 +0000

    Recognise face and ace cards and assign appropriate values, then add the two dealt cards values

--- assets/script.js
+++ assets/script.js
@@ -40,12 +40,76 @@ dealStartCards(shuffled);
 
 function dealStartCards(shuffled) {
     console.log(shuffled);
- //   document.getElementById("player-one");
-    
+    //   set initial dealer total to 0
+    let dealerTot = 0;
+    let p1Tot = 0;
+    //  deal dealer cards and split the information of suits and values
     let dealerCard1 = shuffled[0];
+    let dc1suit = dealerCard1.slice(-1);
+    let dc1val = dealerCard1.slice(0, -1);
+    console.log(dc1val);
+    if (dc1val === "J") {
+        dc1value = 10;
+    } else if (dc1val === "Q") {
+        dc1value = 10;
+    } else if (dc1val === "K") {
+        dc1value = 10;
+    } else if (dc1val === "A") {
+        dc1value = 11;
+    } else {
+        dc1value = +dc1val;
+    }
+    console.log(dc1value);
     let dealerCard2 = shuffled[1];
+    let dc2suit = dealerCard2.slice(-1);
+    let dc2val = dealerCard2.slice(0, -1);
+    console.log(dc2val);
+    if (dc2val === "J") {
+        dc2value = 10;
+    } else if (dc2val === "Q") {
+        dc2value = 10;
+    } else if (dc2val === "K") {
+        dc2value = 10;
+    } else if (dc2val === "A") {
+        dc2value = 11;
+    } else {
+        dc2value = +dc2val;
+    }
+    console.log(dc2value);
+    //  find the dealer's current card total
+    dealerTot = dc1value + dc2value;
+    console.log(dealerTot);
+    // repeat process for player 1
     let p1Card1 = shuffled[2];
+    let p1c1suit = p1Card1.slice(-1);
+    let p1c1val = +p1Card1.slice(0, -1);
+    if (p1c1val === "J") {
+        p1c1value = 10;
+    } else if (p1c1val === "Q") {
+        p1c1value = 10;
+    } else if (p1c1val === "K") {
+        p1c1value = 10;
+    } else if (p1c1val === "A") {
+        p1c1value = 11;
+    } else {
+        p1c1value = +p1c1val;
+    }
     let p1Card2 = shuffled[3];
+    let p1c2suit = p1Card2.slice(-1);
+    let p1c2val = +p1Card2.slice(0, -1);
+    if (p1c2val === "J") {
+        p1c2value = 10;
+    } else if (p1c2val === "Q") {
+        p1c2value = 10;
+    } else if (p1c2val === "K") {
+        p1c2value = 10;
+    } else if (p1c2val === "A") {
+        p1c2value = 11;
+    } else {
+        p1c2value = +p1c2val;
+    }
+    p1Tot = p1c1value + p1c2value;
+    console.log(p1Tot);
     console.log(`The dealer's cards are ${dealerCard1} and ${dealerCard2}`);
     console.log(`The suit of dealer card 1 is ${dealerCard1.slice(-1)} and its value is ${dealerCard1.slice(0,-1)}`)
     console.log(`The player's cards are ${p1Card1} and ${p1Card2}`);
