340e5e6c0fd16a6b27ca773c3c75e77fb222712a
aj mccredie <aj.mccredie@yahoo.co.uk>
2023-07-01 16:27:13 +0000

    Fix error in code for drawCards where cardsPile was defined twice

--- assets/script/script.js
+++ assets/script/script.js
@@ -223,7 +223,6 @@ function reDrawCards() {
     // create new shuffle deck
     var gameDeck = createDeck();
     var shuffledCards = shuffleDeck(gameDeck); 
-    var cardsPile = document.getElementById("cards-pile");
     shuffledCards.forEach(function(card) {
         var cardElement = document.createElement("div");
         cardElement.textContent = card;
